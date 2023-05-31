e92092bd8f7d8f04e74acda62a42cd817b76a195
pratiknimje1999 <132550569+pratiknimje1999@users.noreply.github.com>
2023-05-31 18:07:33 +0530

    Add files via upload

---
+++ FlipClickAction.java
@@ -0,0 +1,18 @@
+package Webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FlipClickAction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.flipkart.com/");
+		driver.findElement(By.cssSelector("img[alt='Grocery']")).click();
+
+	}
+
+}
