65930c42c3c8d88ac16fa8d8df83a1cbe4df5d76
pratiknimje1999 <132550569+pratiknimje1999@users.noreply.github.com>
2023-06-01 21:55:23 +0530

    Update FlipClickAction.java

--- FlipClickAction.java
+++ FlipClickAction.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class FlipClickAction {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.flipkart.com/");
